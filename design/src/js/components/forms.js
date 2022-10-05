1fbe53ea8bb95a7243979b4943c8f444d0b0a7b3
James LeMosy <james@lemosy.com>
2022-10-05 15:37:27 -0500

    Declare '$' as 'jQuery' in JS.

--- design/src/js/components/forms.js
+++ design/src/js/components/forms.js
@@ -1,99 +1,101 @@
-$(document).ready(function() {
-  // Label behavior for Bootstrap Select elements
-  $('.selectpicker').on('changed.bs.select', function(e, clickedIndex, isSelected, previousValue) {
-    if ($(this).val().length > 0) {
-      $(this).parent().next('label').addClass('label--eyebrow').removeClass('label--placeholder');
-    }
-    else {
-      if ($(this).parent().find('.filter-option-inner-inner').text().length == 0) {
-        $(this).parent().next('label').addClass('label--placeholder').removeClass('label--eyebrow');
+(function($){
+  $(document).ready(function() {
+    // Label behavior for Bootstrap Select elements
+    $('.selectpicker').on('changed.bs.select', function(e, clickedIndex, isSelected, previousValue) {
+      if ($(this).val().length > 0) {
+        $(this).parent().next('label').addClass('label--eyebrow').removeClass('label--placeholder');
       }
-    }
-  });
-  
-  // jQuery Validation for forms.
-  $('form.needs-validation').each(function() {
-    var $form = $(this);
-    
-    // Wrap checkbox groups.
-    var groups = $form.find('input[type=checkbox][name], input[type=radio][name]').map(function() {
-      var name = $(this).attr('name');
-      if ($form.find('[name="' + name + '"]').length > 1) {
-        return name;
+      else {
+        if ($(this).parent().find('.filter-option-inner-inner').text().length == 0) {
+          $(this).parent().next('label').addClass('label--placeholder').removeClass('label--eyebrow');
+        }
       }
-    }).get();
-    groups = $.uniqueSort(groups);
-    $.each(groups, function(index, value) {
-      var form_group = '<div class="form__group';
-      if ($form.find('[name="' + value + '"]:required').length > 0) {
-        form_group += ' has-required';
-      }
-      form_group += '" />';
-      $('[name="' + value + '"]').parent('.form__field').wrapAll(form_group);
     });
     
-    $form.validate({
-      debug: true,
-      rules: {
-      },
-      errorClass: 'is-invalid',
-      errorElement: 'div',
-      errorPlacement: function(error, element) {
-        error.addClass('invalid-feedback');
-        // For checkbox and radio groups
-        if (element.attr('type') === 'checkbox' || element.attr('type') === 'radio') {
-          var $name = element.attr('name');
-          if ($form.find('[name="' + $name + '"]').length > 1) {
-            error.insertBefore($('[name="' + $name + '"]:first').parent('.form__field'));
+    // jQuery Validation for forms.
+    $('form.needs-validation').each(function() {
+      var $form = $(this);
+      
+      // Wrap checkbox groups.
+      var groups = $form.find('input[type=checkbox][name], input[type=radio][name]').map(function() {
+        var name = $(this).attr('name');
+        if ($form.find('[name="' + name + '"]').length > 1) {
+          return name;
+        }
+      }).get();
+      groups = $.uniqueSort(groups);
+      $.each(groups, function(index, value) {
+        var form_group = '<div class="form__group';
+        if ($form.find('[name="' + value + '"]:required').length > 0) {
+          form_group += ' has-required';
+        }
+        form_group += '" />';
+        $('[name="' + value + '"]').parent('.form__field').wrapAll(form_group);
+      });
+      
+      $form.validate({
+        debug: true,
+        rules: {
+        },
+        errorClass: 'is-invalid',
+        errorElement: 'div',
+        errorPlacement: function(error, element) {
+          error.addClass('invalid-feedback');
+          // For checkbox and radio groups
+          if (element.attr('type') === 'checkbox' || element.attr('type') === 'radio') {
+            var $name = element.attr('name');
+            if ($form.find('[name="' + $name + '"]').length > 1) {
+              error.insertBefore($('[name="' + $name + '"]:first').parent('.form__field'));
+            }
+            else {
+              error.appendTo(element.closest('.form__field'));
+            }
           }
+          //Default
           else {
             error.appendTo(element.closest('.form__field'));
           }
-        }
-        //Default
-        else {
-          error.appendTo(element.closest('.form__field'));
-        }
-      },
-      highlight: function(element, errorClass) {
-        // For checkbox and radio groups
-        if ($(element).attr('type') === 'checkbox' || $(element).attr('type') === 'radio') {
-          var $name = $(element).attr('name');
-          if ($form.find('[name="' + $name + '"]').length > 1) {
-            $('[name="' + $name + '"]').closest('.form__group').addClass('has-invalid');
-            $('[name="' + $name + '"]').closest('fieldset.is-required').addClass('has-invalid');
+        },
+        highlight: function(element, errorClass) {
+          // For checkbox and radio groups
+          if ($(element).attr('type') === 'checkbox' || $(element).attr('type') === 'radio') {
+            var $name = $(element).attr('name');
+            if ($form.find('[name="' + $name + '"]').length > 1) {
+              $('[name="' + $name + '"]').closest('.form__group').addClass('has-invalid');
+              $('[name="' + $name + '"]').closest('fieldset.is-required').addClass('has-invalid');
+            }
           }
-        }
-        // Bootstrap Select 'mark-required'
-        else if ($(element).is('.selectpicker')) {
-          $(element).closest('.dropdown').addClass('has-invalid');
-        }
-        $(element).addClass(errorClass);
-      },
-      unhighlight: function(element, errorClass) {
-        // For checkbox and radio groups
-        if ($(element).attr('type') === 'checkbox' || $(element).attr('type') === 'radio') {
-          var $name = $(element).attr('name');
-          if ($form.find('[name="' + $name + '"]').length > 1) {
-            $('[name="' + $name + '"]').closest('.form__group').removeClass('has-invalid');
-            $('[name="' + $name + '"]').closest('fieldset.is-required').removeClass('has-invalid');
+          // Bootstrap Select 'mark-required'
+          else if ($(element).is('.selectpicker')) {
+            $(element).closest('.dropdown').addClass('has-invalid');
           }
+          $(element).addClass(errorClass);
+        },
+        unhighlight: function(element, errorClass) {
+          // For checkbox and radio groups
+          if ($(element).attr('type') === 'checkbox' || $(element).attr('type') === 'radio') {
+            var $name = $(element).attr('name');
+            if ($form.find('[name="' + $name + '"]').length > 1) {
+              $('[name="' + $name + '"]').closest('.form__group').removeClass('has-invalid');
+              $('[name="' + $name + '"]').closest('fieldset.is-required').removeClass('has-invalid');
+            }
+          }
+          // Bootstrap Select 'mark-required'
+          else if ($(element).is('.selectpicker')) {
+            $(element).closest('.dropdown').removeClass('has-invalid');
+          }
+          $(element).removeClass(errorClass);
+        },
+        ignoreTitle: true
+      });
+      
+      // Re-validate when changing Bootstrap Select elements.
+      $form.find(".selectpicker").on('changed.bs.select', function(e, clickedIndex, isSelected, previousValue) {
+        if ($(this).valid()) {
+          $(this).closest('.dropdown').removeClass('has-invalid');
         }
-        // Bootstrap Select 'mark-required'
-        else if ($(element).is('.selectpicker')) {
-          $(element).closest('.dropdown').removeClass('has-invalid');
-        }
-        $(element).removeClass(errorClass);
-      },
-      ignoreTitle: true
+      });
+      
     });
-    
-    // Re-validate when changing Bootstrap Select elements.
-    $form.find(".selectpicker").on('changed.bs.select', function(e, clickedIndex, isSelected, previousValue) {
-      if ($(this).valid()) {
-        $(this).closest('.dropdown').removeClass('has-invalid');
-      }
-    });
-    
   });
-});
+})(jQuery);
--- design/src/js/components/site-header.js
+++ design/src/js/components/site-header.js
@@ -1,26 +1,28 @@
-$(document).ready(function () {
-  $('.site-header').each(function () {
-    const stickHeader = () => {
-      let stickyPos = parseInt($(this).css('top'));
-      if ($(this).offset().top - $(window).scrollTop() <= stickyPos) {
-        $(this).addClass('stuck');
-      } else {
-        $(this).removeClass('stuck');
+(function($){
+  $(document).ready(function () {
+    $('.site-header').each(function () {
+      const stickHeader = () => {
+        let stickyPos = parseInt($(this).css('top'));
+        if ($(this).offset().top - $(window).scrollTop() <= stickyPos) {
+          $(this).addClass('stuck');
+        } else {
+          $(this).removeClass('stuck');
+        }
       }
-    }
-    $(document).scroll(stickHeader);
-    
-    let menu = $(this).children('.menu');
-    let breakpoint = menu.css('--breakpoint');
-    const moveAddl = () => {
-      matchMedia('(max-width: ' + breakpoint + ')').addEventListener("change", moveAddl, { once: true });
-      if (window.matchMedia('(max-width: ' + breakpoint + ')').matches) {
-        $(this).children('.site-header__addl').insertAfter($(this));
+      $(document).scroll(stickHeader);
+      
+      let menu = $(this).children('.menu');
+      let breakpoint = menu.css('--breakpoint');
+      const moveAddl = () => {
+        matchMedia('(max-width: ' + breakpoint + ')').addEventListener("change", moveAddl, { once: true });
+        if (window.matchMedia('(max-width: ' + breakpoint + ')').matches) {
+          $(this).children('.site-header__addl').insertAfter($(this));
+        }
+        else {
+          $(this).next('.site-header__addl').appendTo($(this));
+        }
       }
-      else {
-        $(this).next('.site-header__addl').appendTo($(this));
-      }
-    }
-    moveAddl();
-  });  
-});
+      moveAddl();
+    });  
+  });
+})(jQuery);
