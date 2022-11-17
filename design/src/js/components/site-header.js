a2de9ee19aeb7239e84eb958dd610e73681c0947
James LeMosy <james@lemosy.com>
2022-11-16 19:04:08 -0600

    Modified site-header.js to stick to first scrollable parent.

--- design/src/js/components/site-header.js
+++ design/src/js/components/site-header.js
@@ -1,15 +1,30 @@
 (function($){
   $(document).ready(function () {
     $('.site-header').each(function () {
+      $.fn.scrollParent = function() {
+        let position = $(this).css("position");
+        let excludeStaticParent = position === "absolute";
+        let scrollParent = $(this).parents().filter(function() {
+          let parent = $(this);
+          if (excludeStaticParent && parent.css("position") === "static" ) {
+            return false;
+          }
+          return (/(auto|scroll)/).test(parent.css("overflow") + parent.css("overflow-y") + parent.css("overflow-x"));
+        }).eq(0);
+        
+        return position === "fixed" || !scrollParent.length ? $(this[0].ownerDocument || window ) : scrollParent;
+      }
+      
       const stickHeader = () => {
         let stickyPos = parseInt($(this).css('top'));
-        if ($(this).offset().top - $(this).parent().scrollTop() <= stickyPos) {
+        if ($(this).position().top - $(this).scrollParent().scrollTop() <= stickyPos) {
           $(this).addClass('stuck');
         } else {
           $(this).removeClass('stuck');
         }
       }
-      $(document).scroll(stickHeader);
+      
+      $(scrollParent).scroll(stickHeader);
       
       let menu = $(this).children('.menu');
       let breakpoint = menu.css('--breakpoint');
